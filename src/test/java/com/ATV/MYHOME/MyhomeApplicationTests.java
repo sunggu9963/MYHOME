894791afdfca359bfb77b12234c44eefb8d45520
sunggu <seonggubag69@gmail.com>
2021-03-30 17:18:42 +0900

    init

---
+++ src/test/java/com/ATV/MYHOME/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ATV.MYHOME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
